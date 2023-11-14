30e3de8ff0e6e25c57bbe9f529986cfa48888078
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-14 15:10:16 +0100

    Dodanie adresu

--- src/Domain/Model/User.cs
+++ src/Domain/Model/User.cs
@@ -5,5 +5,15 @@ public class User : BaseEntity
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+    public Address ShippingAddress { get; set; } = new();
 
+    public string HashedPassword { get; set; }
+
+}
+
+
+public class Address : Base
+{
+    public string City { get; set; }
+    public string Street { get; set; }
 }
