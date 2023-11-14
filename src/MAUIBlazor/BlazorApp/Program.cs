5b5ceafe0f2192302d95974357eca964426dcc4f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-14 16:03:25 +0100

    Dodanie klienta http

--- src/MAUIBlazor/BlazorApp/Program.cs
+++ src/MAUIBlazor/BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp;
+using BlazorApp.Services;
 using Domain.Abstractions;
 using Domain.Model;
 using Infrastructure;
@@ -9,7 +10,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7014") });
+
+
+// dotnet add package Microsoft.Extensions.Http
+builder.Services.AddHttpClient<UserApiService>(sp => sp.BaseAddress = new Uri("https://localhost:7014"));
 
 builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
 builder.Services.AddSingleton<IEnumerable<User>>(_ => new List<User>
@@ -20,5 +25,7 @@ builder.Services.AddSingleton<IEnumerable<User>>(_ => new List<User>
         });
 
 builder.Services.AddSingleton<IMessageService, FakeMessageService>();
+
+builder.Services.AddScoped<UserApiService>();
 
 await builder.Build().RunAsync();
---
+++ src/MAUIBlazor/BlazorApp/Services/UserApiService.cs
@@ -0,0 +1,11 @@
+﻿using Api.DTO;
+using System.Net.Http.Json;
+
+namespace BlazorApp.Services;
+
+public class UserApiService
+{
+    private readonly HttpClient client;
+    public UserApiService(HttpClient client) => this.client = client;
+    public Task<IEnumerable<UserDTO>?> GetAllAsync() => client.GetFromJsonAsync<IEnumerable<UserDTO>>("/api/users");
+}
