485ed655da42f75bc0956cbd8d5593e00869a6ec
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-14 11:42:35 +0100

    Dodanie FakeMessageService

---
+++ src/Infrastructure/FakeMessageService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Abstractions;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure;
+
+public class FakeMessageService : IMessageService
+{
+    public void Send(Message message)
+    {
+        Console.WriteLine(message);
+    }
+}
