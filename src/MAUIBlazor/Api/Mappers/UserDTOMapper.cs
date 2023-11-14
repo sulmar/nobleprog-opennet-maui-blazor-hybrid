ebf472279a946fe1d9ebd1691b2768680866a8db
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-14 14:26:29 +0100

    Dodanie ręcznego mappera

---
+++ src/MAUIBlazor/Api/Mappers/UserDTOMapper.cs
@@ -0,0 +1,14 @@
+﻿using Api.DTO;
+using Domain.Model;
+
+namespace Api.Mappers;
+
+public static class UserDTOMapper
+{
+    public static UserDTO ToDTO(this User user) => Map(user);
+
+    private static UserDTO Map(User u) => new(
+                       u.FirstName,
+                       u.LastName,
+                       u.Email);
+}
