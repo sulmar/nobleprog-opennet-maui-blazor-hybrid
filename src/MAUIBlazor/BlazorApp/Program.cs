547d69d8d81d6c53b2bc0fede522649c3fcf837d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-11-15 09:13:32 +0100

    Fix

--- src/MAUIBlazor/BlazorApp/Program.cs
+++ src/MAUIBlazor/BlazorApp/Program.cs
@@ -10,7 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7014") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
 // dotnet add package Microsoft.Extensions.Http
